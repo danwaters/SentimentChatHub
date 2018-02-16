bd0f31b9297bcf47bb29c54e7e3450e946502771
Dan Waters <dawate@microsoft.com>
2018-02-16 11:04:37 -0600

    Initial commit
    SignalR (Core) hub for Sentiment chat

---
+++ SentimentChatHub/SentimentChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SentimentChatHub
+{
+    public class SentimentChatHub : Hub
+    {
+        public void NewAnalyzedMessage(string username, string message, double sentiment)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.InvokeAsync("NewAnalyzedMessage", username, message, sentiment);
+        }
+    }
+}
